f94761d9ee20d899193621821f05b03ce3c5a8b8
Ibrahim Adeyemi <hebrospecial@yahoo.com>
2025-05-30 11:50:40 +0100

    Add files via upload

---
+++ atmCard.js
@@ -0,0 +1,62 @@
+let obj = {
+	cardNumber: [4, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2],
+	name : "Ibrahim",
+	isValid : true,
+	isInvalid : false
+
+};
+
+switch(cardnumber){
+	case 1:console.log("invalid card");
+		break;
+	case 2:console.log("invalid card");
+		break;
+	case 3: console.log("American Express");
+		for(index = 0; index <= 14; index++){
+			if index = 14 && index0 = 3{
+		console.log("American Express");
+			}else{ 
+		console.log("invalid card, reason: "Invalid lenght");
+		break;
+          }
+           }
+	case 4: console.log("Visacard");
+		for(index = 0; index <= 15; index++){
+			if index = 15 && index0 = 4 {
+		console.log("Visacard");
+			}else{ 
+		console.log("invalid card, reason: "Invalid lenght");
+		break;
+          }
+           } 
+	case 5: console.log("Mastercard");
+		for(index = 0; index <= 15; index++){
+			if index = 15 && index0 = 5 {
+		console.log("Mastercard");
+			}else{ 
+		console.log("invalid card, reason: "Invalid lenght");
+		break;
+         }
+          }   
+	case 6: console.log("Discover");
+		for(index = 0; index <= 15; index++){
+			if index = 15 && index0 = 6 {
+		console.log("Discover");
+			}else{ 
+		console.log("invalid card, reason: "Invalid lenght");
+		break;
+         }
+          }
+	case 7:	console.log("invalid card");
+		break;
+	case 8: console.log("invalid card");
+		break;
+	case 9: console.log("invalid card");
+		break;
+	case 0: console.log("invalid card");
+		break;
+
+              return cardnumber;
+
+
+}
