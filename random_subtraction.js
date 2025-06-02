0ed85673d598c83599e9a82360b04650ac1dc717
Ibrahim Adeyemi <hebrospecial@yahoo.com>
2025-06-02 16:58:36 +0100

    Add files via upload

---
+++ random_subtraction.js
@@ -0,0 +1,29 @@
+function RandomSubraction(){
+let num1 = Math.floor(Math.Random() * 9)
+let num2 = Math.floor(Math.Random() * 9)
+return num1, num2
+
+};
+
+console.log(correct_responses("Very good!"))
+
+console.log(incorrect_responses("Wrong. Try once more."))
+
+while(true){
+
+    let num1, num2 = generate_question()
+    
+    correct_answer = x - y
+
+};
+    
+    while(true){
+        user_input = ("What is x minus y? ")
+        
+        if (user_input == correct_answer) {
+            console.log(random.choice(correct_responses))
+            break;
+        }else{
+            console.log(random.choice(incorrect_responses))
+
+};
