2f85b8ea5783fc996fe86598d146dbcc42a23d9d
Ibrahim Adeyemi <hebrospecial@yahoo.com>
2025-06-03 16:54:00 +0100

    Add files via upload

---
+++ bankatm.js
@@ -0,0 +1,35 @@
+let balance = 0;
+let withdrawalFee = 100;
+let maxAmount = 20000;
+
+function deposit(amount) {
+	deposit = prompt(float("Amount to deposit: "));
+		if (amount <= 0); {
+			console.log("Deposit amount must be positive.");
+			self.balance += amount;
+			console.log('Your balance is ', balance);
+
+}
+};
+
+function withdraw(amount) {
+	withdraw = prompt(float("Amount to withdraw: "));
+		amount % 500 == 0;
+		max_amount <= 20_000;
+		if (amount <= 0) {
+			console.log("Withdrawal amount must be positive.");
+		if (max_amount > 20_000);
+			console.log("You cannot withdraw more than 20_000 at once.");
+		if (amount > balance);
+			console.log("Insufficient funds.");
+		if (withdraw > balance * 0.9);
+			console.log("Insufficient funds.");
+		if (amount < balance);
+			self.balance = amount - (withdrawal_fee + withdraw);
+			console.log("Transaction succesful!");
+			console.log("Amount withdraw: ", withdraw);
+			console.log("Withdrawal fee: N100");
+			console.log("Remaining balance: ", balance);
+}
+};
+
